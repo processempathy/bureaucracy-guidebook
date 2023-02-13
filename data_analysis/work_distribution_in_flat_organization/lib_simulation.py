c26fe5a17efacec23fb9022b67a7ffea38fcc644
Ben Payne <ben.is.located@gmail.com>
2023-02-12 20:28:25 -0500

    progress, but somewhat confused

--- data_analysis/work_distribution_in_flat_organization/lib_simulation.py
+++ data_analysis/work_distribution_in_flat_organization/lib_simulation.py
@@ -1,4 +1,4 @@
-import random
+import random # don't import random because it overwrites the random seed set in the notebook?
 import numpy
 import pandas
 import sys
@@ -7,6 +7,7 @@ import sys
 module for the simulation of work distribution in a flat organization
 """
 
+random.seed(10)
 
 class CreatePerson():
     """
@@ -43,6 +44,23 @@ def show_all_people(list_of_people):
         print('id=',person.unique_id, 
               '; skill matrix=',person.skill_specialization_dict)
 
+def get_aggregate_person_dict(list_of_people:list,
+                              skill_set_for_people:list,
+                              max_skill_level_per_person: int):
+    # initialize data structure
+    aggregate_person_dict = {}
+    for specialization in skill_set_for_people:
+        aggregate_person_dict[specialization] = [0 for _ in range(max_skill_level_per_person)]
+
+    # populate the data structure
+    for person_id, person in enumerate(list_of_people):
+        for specialization, skilllevel in person.skill_specialization_dict.items():
+            #print(person_id, specialization, skilllevel)
+            if skilllevel>0:
+                aggregate_person_dict[specialization][skilllevel-1] += 1
+                
+    return aggregate_person_dict
+        
 def check_population_for_capability(list_of_people,
                                     skill_set_for_people,
                                     max_skill_level_per_person):
@@ -131,7 +149,9 @@ def pick_a_random_person(person_index: int,
     return None
 
 
-def simulate(skill_set_for_tasks:list,
+def simulate(show_narrative:bool,
+             work_journal:bool,
+             skill_set_for_tasks:list,
              max_skill_level_per_task: int,
              max_ticks_to_simulate: int,
              max_task_duration_in_ticks:int,
@@ -142,30 +162,30 @@ def simulate(skill_set_for_tasks:list,
     tick=-1
     while ((tick<max_ticks_to_simulate)):
         tick=tick+1
-        print('\n===== tick',tick,'=====')
-        print("   ===== status at the leading edge of this tick: =====")
-        current_status_of_people(list_of_people)
-        print("   ===== updates happening in this tick: =====")
+        if show_narrative: print('\n===== tick',tick,'=====')
+        if show_narrative: print("   ===== status at the leading edge of this tick: =====")
+        if show_narrative: current_status_of_people(list_of_people)
+        if show_narrative: print("   ===== updates happening in this tick: =====")
         # each person looks in their backlog for work
         for person_index in range(len(list_of_people)):
             skill_dict = list_of_people[person_index].skill_specialization_dict
-            print("person",person_index,'has skills',skill_dict)
+            if show_narrative: print("person",person_index,'has skills',skill_dict)
 
             # initialize the work journal        
-            list_of_people[person_index].work_journal_per_tick[tick] = {'number of tasks in backlog': 
+            if work_journal: list_of_people[person_index].work_journal_per_tick[tick] = {'number of tasks in backlog': 
                                                                         len(list_of_people[person_index].backlog_of_tasks)}
-            print("   person",person_index,"has backlog length",len(list_of_people[person_index].backlog_of_tasks))
+            if show_narrative: print("   person",person_index,"has backlog length",len(list_of_people[person_index].backlog_of_tasks))
 
             # make sure the idle people get assigned work
             if list_of_people[person_index].status=="idle":
-                list_of_people[person_index].work_journal_per_tick[tick]['status was'] = "idle"
+                if work_journal: list_of_people[person_index].work_journal_per_tick[tick]['status was'] = "idle"
                 assert(list_of_people[person_index].assigned_task is None)
                 if len(list_of_people[person_index].backlog_of_tasks)>0:
                     # new tasks get appended, so the oldest task is at position 0
                     list_of_people[person_index].assigned_task = list_of_people[person_index].backlog_of_tasks.pop(0)
-                    print("   person",person_index,"got task from their backlog")
-                    print("      task is",list_of_people[person_index].assigned_task)
-                    list_of_people[person_index].work_journal_per_tick[tick]['task from'] = 'my own backlog'
+                    if show_narrative: print("   person",person_index,"got task from their backlog")
+                    if show_narrative: print("      task is",list_of_people[person_index].assigned_task)
+                    if work_journal: list_of_people[person_index].work_journal_per_tick[tick]['task from'] = 'my own backlog'
                 else: # pop from empty list
                     task_id = max(tasks_dict.keys())+1
                     tasks_dict = new_task(task_id,
@@ -174,29 +194,29 @@ def simulate(skill_set_for_tasks:list,
                                           max_task_duration_in_ticks,
                                           tasks_dict)                    
                     list_of_people[person_index].assigned_task = tasks_dict[task_id]
-                    print("   person",person_index,"had no tasks in backlog; got new task from infinite queue")
-                    print("      task consists of",list_of_people[person_index].assigned_task)
-                    list_of_people[person_index].work_journal_per_tick[tick]['task from'] = 'infinite backlog'
+                    if show_narrative: print("   person",person_index,"had no tasks in backlog; got new task from infinite queue")
+                    if show_narrative: print("      task consists of",list_of_people[person_index].assigned_task)
+                    if work_journal: list_of_people[person_index].work_journal_per_tick[tick]['task from'] = 'infinite backlog'
 
             # at this point, regardless of status, the person has a task
             if not list_of_people[person_index].assigned_task:
                 raise Exception("person",person_index,"should have a task")
                 
             my_task = list_of_people[person_index].assigned_task
-            print("   person",person_index,"has an assigned task",my_task)
-            list_of_people[person_index].work_journal_per_tick[tick]['task'] = my_task
+            if show_narrative: print("   person",person_index,"has an assigned task",my_task)
+            if work_journal: list_of_people[person_index].work_journal_per_tick[tick]['task'] = my_task
             
             # can the person do the task, or do they need to coordinate?
             if (my_task['skill level'] <= skill_dict[my_task['specialization']]): # person can do task
-                print("   person",person_index,"can do the task!")
+                if show_narrative: print("   person",person_index,"can do the task!")
                 list_of_people[person_index].status="working"
-                list_of_people[person_index].work_journal_per_tick[tick]['status was'] = "idle"
-                list_of_people[person_index].work_journal_per_tick[tick]['status is now'] = "working"
+                if work_journal: list_of_people[person_index].work_journal_per_tick[tick]['status was'] = "idle"
+                if work_journal: list_of_people[person_index].work_journal_per_tick[tick]['status is now'] = "working"
             else: # person doesn't have sufficient skill for task; person needs to find someone else
-                print("   person",person_index,"does not have sufficient skill")
+                if show_narrative: print("   person",person_index,"does not have sufficient skill")
                 list_of_people[person_index].status="coordinating"
-                list_of_people[person_index].work_journal_per_tick[tick]['status was'] = "idle"
-                list_of_people[person_index].work_journal_per_tick[tick]['status is now'] = "coordinating"
+                if work_journal: list_of_people[person_index].work_journal_per_tick[tick]['status was'] = "idle"
+                if work_journal: list_of_people[person_index].work_journal_per_tick[tick]['status is now'] = "coordinating"
 
             # no person is idle
             assert((list_of_people[person_index].status=="working") or 
@@ -204,34 +224,35 @@ def simulate(skill_set_for_tasks:list,
 
             if list_of_people[person_index].status=="working":
                 speedup = skill_dict[my_task['specialization']]/my_task['skill level']
-                print("   speedup for person",person_index,"is", speedup)
+                if show_narrative: print("   speedup for person",person_index,"is", speedup)
                 list_of_people[person_index].assigned_task['remaining'] = my_task['remaining'] - speedup # "doing the work"
                 if (list_of_people[person_index].assigned_task['remaining']<=0): # task was completed
-                    print("   task completed!")
+                    if show_narrative: print("   task completed!")
                     list_of_people[person_index].assigned_task = None
                     list_of_people[person_index].status="idle"
-                    list_of_people[person_index].work_journal_per_tick[tick]['outcome'] = "task completed"
-                    list_of_people[person_index].work_journal_per_tick[tick]['status is now'] = "idle"
+                    if work_journal: list_of_people[person_index].work_journal_per_tick[tick]['outcome'] = "task completed"
+                    if work_journal: list_of_people[person_index].work_journal_per_tick[tick]['status is now'] = "idle"
                 else: # update the task to reflect there being less work remaining because the person did some work
-                    list_of_people[person_index].work_journal_per_tick[tick]['outcome'] = "worked but task remains"
-                    list_of_people[person_index].work_journal_per_tick[tick]['status is now'] = "working"
+                    if work_journal: list_of_people[person_index].work_journal_per_tick[tick]['outcome'] = "worked but task remains"
+                    if work_journal: list_of_people[person_index].work_journal_per_tick[tick]['status is now'] = "working"
+                    pass
                     
             if list_of_people[person_index].status=="coordinating":
                 contacts = list_of_people[person_index].contact_list
-                print("   person",person_index,"contacts=",contacts)
+                if show_narrative: print("   person",person_index,"contacts=",contacts)
                 if len(contacts)>0: # I know people!
-                    print("   person",person_index,"looks in contact list")
+                    if show_narrative: print("   person",person_index,"looks in contact list")
                     for contact_id in contacts: # do the people I know have the skills to do this task?
                         contacts_skill_dict = list_of_people[contact_id].skill_specialization_dict
                         if (my_task['skill level'] <= contacts_skill_dict[my_task['specialization']]): # contact can do task
                             list_of_people[contact_id].backlog_of_tasks.append(list_of_people[person_index].assigned_task)
                             list_of_people[person_index].assigned_task = None
                             list_of_people[person_index].status = "idle" # next tick will get new task from my own backlog or infinite backlog
-                            print("   person",person_index,"gave task to person",contact_id,"from contact list")
-                            list_of_people[person_index].work_journal_per_tick[tick]['outcome'] = "gave task to person "+str(contact_id)+" from contact list"
+                            if show_narrative: print("   person",person_index,"gave task to person",contact_id,"from contact list")
+                            if work_journal: list_of_people[person_index].work_journal_per_tick[tick]['outcome'] = "gave task to person "+str(contact_id)+" from contact list"
                             list_of_people[person_index].add_person_to_contact_list(contact_id)
-                            print("   person",person_index,"added",contact_id,"to list of contacts")
-                            list_of_people[person_index].work_journal_per_tick[tick]['status is now'] = "idle"
+                            if show_narrative: print("   person",person_index,"added",contact_id,"to list of contacts")
+                            if work_journal: list_of_people[person_index].work_journal_per_tick[tick]['status is now'] = "idle"
                 # after looking through contacts, if the status is still "coordinating", 
                 # then person didn't have a contact who could do the work
                 if list_of_people[person_index].status=="coordinating": 
@@ -241,13 +262,13 @@ def simulate(skill_set_for_tasks:list,
                         list_of_people[another_person_id].backlog_of_tasks.append(my_task)
                         list_of_people[person_index].assigned_task = None
                         list_of_people[person_index].status = "idle"
-                        print("   person",person_index,"gave task to person",another_person_id,"from random search")
-                        list_of_people[person_index].work_journal_per_tick[tick]['outcome'] = "gave task to person "+str(another_person_id)+" from random search"
-                        list_of_people[person_index].work_journal_per_tick[tick]['status is now'] = "idle"
+                        if show_narrative: print("   person",person_index,"gave task to person",another_person_id,"from random search")
+                        if work_journal: list_of_people[person_index].work_journal_per_tick[tick]['outcome'] = "gave task to person "+str(another_person_id)+" from random search"
+                        if work_journal: list_of_people[person_index].work_journal_per_tick[tick]['status is now'] = "idle"
                     else:
-                        print("   person",person_index,"not able give to",another_person_id,"from random search")
-                        list_of_people[person_index].work_journal_per_tick[tick]['outcome'] = "not able to give to person "+str(another_person_id)+" from random search"
-                        list_of_people[person_index].work_journal_per_tick[tick]['status is now'] = "coordinating"
+                        if show_narrative: print("   person",person_index,"not able give to",another_person_id,"from random search")
+                        if work_journal: list_of_people[person_index].work_journal_per_tick[tick]['outcome'] = "not able to give to person "+str(another_person_id)+" from random search"
+                        if work_journal: list_of_people[person_index].work_journal_per_tick[tick]['status is now'] = "coordinating"
 
     return list_of_people, tasks_dict
 
