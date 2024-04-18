6e0709fe6d67f1f9a6d6a302e892fea85f112bea
Ben Payne <ben.is.located@gmail.com>
2024-04-18 05:10:36 -0400

    tweaks

---
+++ data_analysis/work_distribution_in_flat_organization/configuration.py
@@ -0,0 +1,15 @@
+skill_set_for_people = ['A','B','C'] # uniform distribution
+skill_set_for_tasks  = skill_set_for_people
+max_skill_level_per_person      = 3  # uniform distribution
+max_skill_level_per_task        = max_skill_level_per_person
+max_task_duration_in_ticks      = 5  # uniform distribution
+
+min_number_of_people = 20
+max_number_of_people = 20
+
+min_social_circle_size = 2
+max_social_circle_size = 20
+
+max_ticks_to_simulate = 100
+
+number_of_simulations = 25
