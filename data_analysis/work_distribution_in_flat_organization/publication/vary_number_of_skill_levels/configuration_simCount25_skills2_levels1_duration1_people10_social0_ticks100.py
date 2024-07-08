592cdebc77c2ec810919d8ead6f2561698734212
Ben Payne <ben.is.located@gmail.com>
2024-07-07 21:34:54 -0400

    better pictures

---
+++ data_analysis/work_distribution_in_flat_organization/publication/vary_number_of_skill_levels/configuration_simCount25_skills2_levels1_duration1_people10_social0_ticks100.py
@@ -0,0 +1,17 @@
+skill_set_for_people = ['A','B'] # ['A','B','C'] # uniform distribution
+skill_set_for_tasks  = skill_set_for_people
+min_skill_level_per_person      = 1  # uniform distribution
+min_skill_level_per_task        = min_skill_level_per_person
+max_skill_level_per_person      = 12  # uniform distribution
+max_skill_level_per_task        = max_skill_level_per_person
+max_task_duration_in_ticks      = 1  # uniform distribution
+
+min_number_of_people = 10
+max_number_of_people = 10
+
+min_social_circle_size = 0
+max_social_circle_size = 0
+
+max_ticks_to_simulate = 100
+
+number_of_simulations = 50
